c39ed27bb3f240c4423dae6280e838b9ac10cb11
Hasmik-Hohannisyan <82596963+Hasmik-Hohannisyan@users.noreply.github.com>
2021-08-09 23:31:42 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework1_HS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+
+        static void MyMain()
+        {
+            Console.WriteLine("Hello Has");
+
+            Console.ReadKey();
+        }
+    }
+}
